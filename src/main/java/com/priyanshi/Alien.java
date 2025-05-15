a80e73c454364ab4863775d778a3cd3e6484ab03
priyanshi1399 <priyanshitomar001@gmail.com>
2025-05-15 17:58:18 +0530

    one Parameter by constructor-org

--- src/main/java/com/priyanshi/Alien.java
+++ src/main/java/com/priyanshi/Alien.java
@@ -2,9 +2,14 @@ package com.priyanshi;
 
 public class Alien {
     private int age;
-    Laptop lap;
+    private Laptop lap;
     public  Alien(){
         System.out.println("Object Created");
+    }
+
+    public Alien(int age) {
+        System.out.println("Paramerterized constructor called");
+        this.age = age;
     }
 
     public int getAge() {
