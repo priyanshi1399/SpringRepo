e8e26a06fc64ebdb5fc6871d5ed6ec5093853130
priyanshi1399 <priyanshitomar001@gmail.com>
2025-05-15 17:51:43 +0530

    Inject Reference

--- src/main/java/com/priyanshi/Alien.java
+++ src/main/java/com/priyanshi/Alien.java
@@ -2,7 +2,7 @@ package com.priyanshi;
 
 public class Alien {
     private int age;
-
+    Laptop lap;
     public  Alien(){
         System.out.println("Object Created");
     }
@@ -16,7 +16,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
--- src/main/java/com/priyanshi/Laptop.java
+++ src/main/java/com/priyanshi/Laptop.java
@@ -4,4 +4,8 @@ public class Laptop {
     public Laptop(){
         System.out.println("Laptop Object Created");
     }
+
+    public void compile(){
+        System.out.println("compiling....");
+    }
 }
