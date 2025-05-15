63498cd1a41c68948b2a24d8c007382be0abcae3
priyanshi1399 <priyanshitomar001@gmail.com>
2025-05-15 17:36:09 +0530

    Setting Injection

--- src/main/java/com/priyanshi/Alien.java
+++ src/main/java/com/priyanshi/Alien.java
@@ -1,11 +1,21 @@
 package com.priyanshi;
 
 public class Alien {
-    int age;
+    private int age;
 
     public  Alien(){
         System.out.println("Object Created");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setting the value");
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("Coding");
     }
--- src/main/java/com/priyanshi/App.java
+++ src/main/java/com/priyanshi/App.java
@@ -13,13 +13,13 @@ public class App
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
         Alien obj1=(Alien)context.getBean("alien1");
-        obj1.age=21;
-        System.out.println(obj1.age);
-       // obj1.code();
+        //obj1.setAge(21);
+        System.out.println(obj1.getAge());
+        obj1.code();
 
-        Alien obj2=(Alien)context.getBean("alien1");
-
-        System.out.println(obj2.age);
+//        Alien obj2=(Alien)context.getBean("alien1");
+//
+//        System.out.println(obj2.age);
        // obj2.code();
     }
 }
